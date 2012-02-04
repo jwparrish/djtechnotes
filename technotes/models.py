6ea81c98dc9df209383c44fd276d60e3e312c90d
Jon Parrish <jonparrish@rocketmail.com>
2012-02-04 15:01:06 -0600

    Removed join table, added foreign key to User table from Note table, need to fix views, templates, and urlconf for change

--- technotes/models.py
+++ technotes/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 
 class Note(models.Model):
 	note = models.TextField()
+	user = models.ForeignKey(User)
 	
+
+"""
 class Bookmark(models.Model):
 	title = models.CharField(max_length=200)
 	user = models.ForeignKey(User)
 	note = models.ForeignKey(Note)
-	
+"""
