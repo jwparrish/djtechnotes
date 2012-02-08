f4e0d17d1082f1bc77b478a5fc99164e31508847
Jon Parrish <jonparrish@rocketmail.com>
2012-02-08 15:01:21 -0600

    Added form validation for username and password

--- technotes/forms.py
+++ technotes/forms.py
@@ -1,3 +1,6 @@
+import re
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django import forms
 
 class RegistrationForm(forms.Form):
@@ -11,3 +14,21 @@ class RegistrationForm(forms.Form):
 		label='Password (Again)',
 		widget=forms.PasswordInput()
 	)
+	
+	def clean_password(self):
+		if 'password1' in self.cleaned_data:
+			password1 = self.cleaned_data['password1']
+			password2 = self.cleaned_data['password2']
+			if password1 == password2:
+				return password2
+		raise forms.ValidationError('Passwords do not match.')
+	
+	def clean_username(self):
+		username = self.cleaned_data['username']
+		if no re.search(r'^\w+$', username):
+			raise forms.ValidationError('Username can only contain alphanumeric characters and the underscore.')
+			try:
+				User.objects.get(username=username)
+			except ObjectDoesNotExist:
+				return username
+			raise forms.ValidationError('Username is already taken!')
