b0d41963f6e575fe2b4258201c396984b63d4ba3
Ben Keating <ben@deepdream.com>
2012-03-31 11:13:54 -0700

    Moved search up to navbar. Moved the ``external`` template block to the bottom of the page (mostly JS).

---
+++ static/js/search.js
@@ -0,0 +1,11 @@
+function search_submit() {
+	var query = $("#id_query").val();
+	$("#search-results").load(
+	"/search/?ajax&query=" + encodeURIComponent(query)
+	);
+	return false;
+}
+
+$(document).ready(function() {
+	$("#search-form").submit(search_submit);
+});
