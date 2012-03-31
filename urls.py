8e9fcb2c45423f535c0af0a5b44b749428b8c610
Ben Keating <ben@deepdream.com>
2012-03-31 13:28:14 -0700

    Search in navbar works. Added admin link to menu.
    navbar search is having issues displaying ``request.GET``. value… not
    sure what the issue is. Added some styling to the main page. Form
    buttons are now styled. urls.py has been cleaned up.

--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
-from django.contrib.auth.views import logout
-from technotes.views import *
 import os.path
+
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.contrib.auth.views import logout
+from django.views.generic.simple import direct_to_template
+
+from technotes.views import *
 
 site_media = os.path.join(os.path.dirname(__file__), 'site_media')
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -32,20 +33,3 @@ urlpatterns = patterns('',
 	# FAKE REDIRECT FOR NOT AUTHENTICATED USERS
 #	(r'^(?P<path>.+)$', fake_redirect),
 )
-
-
-""" ORIGINAL URL EXAMPLES """
-
-    # Examples:
-    # url(r'^$', 'djtechnotes.views.home', name='home'),
-    # url(r'^djtechnotes/', include('djtechnotes.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-#)
-
-
-
