9a981b74ca3294691581200e0043fcda3f3944fe
Jon Parrish <jonparrish@rocketmail.com>
2012-06-07 15:51:57 -0500

    added dbcaching

--- technotes/tests.py
+++ technotes/tests.py
@@ -1,16 +1,17 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ViewTest(TestCase):
+	def setUp(self):
+		self.client = Client()
+		
+	def test_register_page(self):
+		data = {
+			'username': 'test_user',
+			'email': 'test_user@example.com',
+			'password1': 'pass1234',
+			'password2': 'pass1234',
+		}
+		response = self.client.post('/register/', data)
+		self.assertEqual(response.status_code, 302)
+		
